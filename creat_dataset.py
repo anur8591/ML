8ae7196f59ac6790a094ebeffe3bbae076ec8583
Anurag Pal <palanurag595@gmail.com>
2026-06-30 20:54:28 +0530

    enter your api keys

--- creat_dataset.py
+++ creat_dataset.py
@@ -2,38 +2,38 @@
 import pandas as pd
 import requests
 
-url = "https://api.themoviedb.org/3/movie/popular?api_key=84143a5419aad4efefa024f3c1a2c78a"
+url = "https://api.themoviedb.org/3/movie/popular?api_key="
 response = requests.get(url)
 
 df = pd.DataFrame() 
 
 for i in range(1,1001):
-  response = requests.get("https://api.themoviedb.org/3/movie/popular?api_key=84143a5419aad4efefa024f3c1a2c78a")
+  response = requests.get("https://api.themoviedb.org/3/movie/popular?api_key=")
   a_temp_df = pd.DataFrame(response.json()['results'])
   df = pd.concat([df, a_temp_df], ignore_index=True)
 
 for i in range(1001,2001):
-  response = requests.get("https://api.themoviedb.org/3/movie/popular?api_key=84143a5419aad4efefa024f3c1a2c78a")
+  response = requests.get("https://api.themoviedb.org/3/movie/popular?api_key=")
   b_temp_df = pd.DataFrame(response.json()['results'])
   df = pd.concat([df, b_temp_df], ignore_index=True)
   
 for i in range(2001,3001):
-  response = requests.get("https://api.themoviedb.org/3/movie/popular?api_key=84143a5419aad4efefa024f3c1a2c78a")
+  response = requests.get("https://api.themoviedb.org/3/movie/popular?api_key=")
   c_temp_df = pd.DataFrame(response.json()['results'])
   df = pd.concat([df, c_temp_df], ignore_index=True)
 
 for i in range(3001,4001):
-  response = requests.get("https://api.themoviedb.org/3/movie/popular?api_key=84143a5419aad4efefa024f3c1a2c78a")
+  response = requests.get("https://api.themoviedb.org/3/movie/popular?api_key=")
   d_temp_df = pd.DataFrame(response.json()['results'])
   df = pd.concat([df, d_temp_df], ignore_index=True)
 
 for i in range(4001,5001):
-  response = requests.get("https://api.themoviedb.org/3/movie/popular?api_key=84143a5419aad4efefa024f3c1a2c78a")
+  response = requests.get("https://api.themoviedb.org/3/movie/popular?api_key=")
   e_temp_df = pd.DataFrame(response.json()['results'])
   df = pd.concat([df, e_temp_df], ignore_index=True)
 
 for i in range(5001,5762):
-  response = requests.get("https://api.themoviedb.org/3/movie/popular?api_key=84143a5419aad4efefa024f3c1a2c78a")
+  response = requests.get("https://api.themoviedb.org/3/movie/popular?api_key=")
   f_temp_df = pd.DataFrame(response.json()['results'])
   df = pd.concat([df, f_temp_df], ignore_index=True)
 
